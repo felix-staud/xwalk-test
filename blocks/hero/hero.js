8c57d68f503e4688f65334811396322ea8353de2
Felix Staud <felix.staud@headwire.com>
2024-04-03 14:19:53 -0700

    adjust hero aueType

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,7 +3,7 @@
  */
 export default function decorate(block) {
   block.dataset.aueResource = 'urn:fcsconnection:core/franklin/components/block/v1/block';
-  block.dataset.aueType = 'component';
+  block.dataset.aueType = 'hero';
 
   const h1 = block.querySelector('h1');
   h1.dataset.aueProp = 'text';
