90f4dd5c3b3fa830167725a130d721c3182b3bc8
Felix Staud <felix.staud@headwire.com>
2024-04-03 14:15:59 -0700

    add aueProp to h1 of hero block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -4,4 +4,8 @@
 export default function decorate(block) {
   block.dataset.aueResource = 'urn:fcsconnection:core/franklin/components/block/v1/block';
   block.dataset.aueType = 'component';
+
+  const h1 = block.querySelector('h1');
+  h1.dataset.aueProp = 'text';
+  h1.dataset.aueType = 'text';
 }
