c6c806dce81e7fa0c1e0d6fee5335d46d1ee888d
Felix Staud <felix.staud@headwire.com>
2024-04-03 09:09:20 -0700

    move hero aue attribute onto block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,7 +2,6 @@
  * @param {HTMLElement} block
  */
 export default function decorate(block) {
-  const h1 = block.querySelector('h1');
-  h1.dataset.aueResource = 'urn:fcsconnection:core/franklin/components/block/v1/block';
-  h1.dataset.aueType = 'component';
+  block.dataset.aueResource = 'urn:fcsconnection:core/franklin/components/block/v1/block';
+  block.dataset.aueType = 'component';
 }
