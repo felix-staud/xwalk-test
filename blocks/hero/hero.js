e90d50a7c5857a318e27395e379df4d1ee7d95bd
Felix Staud <felix.staud@headwire.com>
2024-04-04 14:47:35 -0700

    experimenting: AUE (8)

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,25 +3,16 @@
  */
 export default function decorate(block) {
   // block.dataset.aueResource = 'urn:aemconnection:/core/franklin/components/block/v1/block';
-  block.dataset = {
-    ...block.dataset,
-    // aueType: 'component',
-    aueModel: 'hero',
-  };
+  block.dataset.aueModel = 'hero';
+  // block.dataset.aueType = 'component';
 
   /** @type {HTMLHeadingElement & AUE.PropAttributes} */
   const h1 = block.querySelector('h1');
-  h1.dataset = {
-    ...h1.dataset,
-    aueProp: 'text',
-    // aueType: 'richtext',
-  };
+  h1.dataset.aueProp = 'text';
+  h1.dataset.aueType = 'richtext';
 
   /** @type {HTMLImageElement & AUE.PropAttributes} */
   const img = block.querySelector('img');
-  img.dataset = {
-    ...img.dataset,
-    aueProp: 'image',
-    // aueType: 'reference',
-  };
+  img.dataset.aueProp = 'image';
+  // img.dataset.aueType = 'reference';
 }
