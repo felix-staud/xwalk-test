b0eccbbe41d9bc5e57fcfb9bbe6990ff943b52bf
Felix Staud <felix.staud@headwire.com>
2024-04-04 14:49:21 -0700

    experimenting: AUE (10)

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -5,6 +5,7 @@ export default function decorate(block) {
   // block.dataset.aueResource = 'urn:aemconnection:/core/franklin/components/block/v1/block';
   block.dataset.aueModel = 'hero';
   block.dataset.aueType = 'component';
+  block.dataset.aueLabel = 'Hero';
 
   /** @type {HTMLHeadingElement & AUE.PropAttributes} */
   const h1 = block.querySelector('h1');
